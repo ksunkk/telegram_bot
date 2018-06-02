f70dbf2f29446e786af923e3bafe7e99a408ef09
ksunkk <ksunkk@outllok.com>
2018-06-02 16:33:41 +0300

    fix

--- db/migrate/20180520183620_add_roles.rb
+++ db/migrate/20180520183620_add_roles.rb
@@ -1,6 +1,6 @@
 class AddRoles < ActiveRecord::Migration[5.2]
   def change
     roles = YAML.load_file('config/roles.yml')
-    roles.each { |role_params| Role.create(role_params) unless Role.find(role_params[:id]).present? }
+    roles.each { |role_params| Role.create(role_params) unless Role.find(role_params['id']).present? }
   end
 end
